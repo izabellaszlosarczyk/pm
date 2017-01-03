9552b0a7234dc8c23616244e5cd4df9f27ec7719
Zuchens <paulina.zak1@gmail.com>
2017-01-03 01:22:51 +0100

    Apriori and decsiion tree

--- analytics/polls/apriori.py
+++ analytics/polls/apriori.py
@@ -1,3 +1,41 @@
+from apyori import apriori
+import json
+# min_support
+# min_confidence
+# min_lift
+# max_length
 
-def apriori_rules(data,desc):
-    return data
+def apriori_rules(data,desc_json):
+    desc_json = json.loads(desc_json)
+    rows = data.split('\n')
+    rows = [x.split(",") for x in rows ]
+    desc = rows[0]
+    data = rows[1:]
+    dataset = []
+    for row in data:
+        set  = []
+        for item in range (0,len(desc)):
+            if int(row[item])==0:
+                set.append(desc_json['answers'][item]+"=0")
+            else:
+                set.append(desc_json['answers'][item]+"=1")
+        dataset.append(set)
+    results = list(apriori(dataset,min_support=0.3,min_confidence=0.5))
+    ordered = [r.ordered_statistics for r in results]
+    order  = [item for sublist in ordered for item in sublist]
+    rules = []
+    for item in order:
+        if len(item.items_base)>0:
+            js_dumps =json.dumps({'items_base':[x for x in item.items_base], 'items_add':[x for x in item.items_add],'confidence':item.confidence,'lift':item.lift})
+            rules.append(js_dumps)
+    desc_json['rules']=rules
+    return desc_json
+
+#
+# f = open('/home/zuchens/Desktop/pm/analytic_module/input_data/apriori.json','rb')
+# desc = f.read()
+#
+# f = open('/home/zuchens/Desktop/pm/analytic_module/input_data/apriori.csv', 'rb')
+# data = f.read()
+#
+# apriori_rules(data,desc)
